c66f77462c028e876fb63b32168cc27e5fa5ccff
Krunal Shah <shahkrunal7091@gmail.com>
2025-12-03 21:29:00 +0530

    Add Country and Language models, migrations, and seeders
    - Created Country and Language models with mass assignable attributes.
    - Added migrations for countries and languages tables.
    - Implemented seeders for populating countries and languages data.
    - Updated DatabaseSeeder to include CountrySeeder and LanguageSeeder.

---
+++ database/migrations/2025_12_03_154947_create_countries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('flag', 10);
+            $table->string('code', 2)->unique();
+            $table->string('dial_code', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
