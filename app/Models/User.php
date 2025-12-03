e7a32afeade1474535efb42b99017d13f2f188df
Krunal Shah <shahkrunal7091@gmail.com>
2025-12-03 21:16:01 +0530

    Remove Laravel Sanctum from the project, including its configuration and related dependencies. Update user model and API routes to reflect the removal of Sanctum authentication.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
